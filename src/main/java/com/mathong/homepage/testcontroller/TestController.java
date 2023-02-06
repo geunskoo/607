1c18d005ed09f3adde2ae6f4d4b7f7547390ceb3
Tae Geun Kim <oryukdo3@pusan.ac.kr>
2023-02-06 16:51:16 +0900

    docs: 설명추가

--- src/main/java/com/mathong/homepage/testcontroller/TestController.java
+++ src/main/java/com/mathong/homepage/testcontroller/TestController.java
@@ -3,6 +3,10 @@ package com.mathong.homepage.testcontroller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Spring framework 와 react 를 처음 연결해보는 영광스러운
+ * Controller
+ */
 @RestController
 public class TestController {
     @GetMapping("/hello")
