53dfd83620398ae1c2fd029a0898de063123c49f
GangHub1970 <dbsrkd1970@pusan.ac.kr>
2023-02-07 16:16:58 +0900

    feat: 구글 로그인 프론트에서 요청만 하는 버젼

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,29 +1,11 @@
-import logo from "./logo.svg";
 import "./App.css";
-import { useEffect, useState } from "react";
-import axios from "axios";
+import GoogleLogin from "./components/GoogleLogin/GoogleLogin";
 
 function App() {
-    const [text, setText] = useState('');
-    const handleClick1 = async () => {
-        const res = await axios.get('/test1');
-        const data = await res.data;
-        console.log(data);
-        setText(data)
-    }
-    const handleClick2 = async () => {
-        const res = await axios.get('/test2');
-        const data = await res.data;
-        console.log(data);
-        setText(data)
-    }
-
   return (
-    <div className="App">
-      <button onClick={handleClick1}>test1</button>
-      <button onClick={handleClick2}>test2</button>
-        <span>{text}</span>
-    </div>
+    <>
+      <GoogleLogin />
+    </>
   );
 }
 
